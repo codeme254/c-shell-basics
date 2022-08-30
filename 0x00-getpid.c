bd253a947337917af9b954a473a8c0b08228c4aa
codeme254 <otwomadennis365@gmail.com>
2022-08-30 13:34:20 +0300

    getting process id

---
+++ 0x00-getpid.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  *main - gets the process id
+  *Return: 0 success
+  */
+
+int main(void)
+{
+	pid_t my_pid;
+
+	my_pid = getpid();
+	printf("%u\n", my_pid);
+	return (0);
+}
+
